bb66d6537b00d6f74e9fec5b52f99ad8a046d98a
ACH ALFARIZI AL-GHAZALI_D3RPLA02 <91650977+alffarizi@users.noreply.github.com>
2021-09-30 22:29:18 +0700

    Profile.java

---
+++ JURNAL 002/Profile.java
@@ -0,0 +1,8 @@
+public class Profile {
+	
+		public static void main(String[] args) {
+			System.out.println("Nama : Ach Alfarizi Al-Ghazali!");
+			System.out.println("NIM : 6706210121");
+			System.out.println("Kelas : D3 RPLA 02");
+		}
+}
